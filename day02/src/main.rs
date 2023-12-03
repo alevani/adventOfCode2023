b19203334c5ff6190165da992a2c554f2b8c6eae
alevani <vanini.alexandre@gmail.com>
2023-12-03 10:25:18 +0100

    Day02 - part 1

---
+++ day02/src/main.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    // File hosts.txt must exist in the current path
+    if let Ok(lines) = read_lines("./input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        // Create a regex pattern to match the specified format
+        let mut result = 0;
+        for (game_id, line) in lines.flatten().enumerate() {
+            
+            // not all of this is necessary
+            let mut nl = line.replace(' ', "").replace("reen", "").replace("lue", "").replace("ed", "");
+            let game_id_det_index = line.find(':').unwrap();
+            nl.replace_range(..game_id_det_index, "");
+            
+            let mut is_game_valid = true;
+            for set in nl.split(';') {
+                let mut blue = 0;
+                let mut green = 0;
+                let mut red = 0;
+
+                for value in set.split(',') {
+                    println!("{value}");
+                    if value.contains('r') {
+                        red = value.split('r').next().unwrap().parse::<i32>().unwrap();
+                    } else if value.contains('b') {
+                        
+                        blue = value.split('b').next().unwrap().parse::<i32>().unwrap();
+                        
+                    } else {
+                        green = value.split('g').next().unwrap().parse::<i32>().unwrap();
+                    }
+                }
+
+                if blue > 14 || red > 12 || green > 13 {
+                    is_game_valid = false;
+                }
+            }
+            
+            if is_game_valid {
+                result += game_id + 1;
+            }
+        }
+        println!("{result}");
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
