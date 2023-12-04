b692911d6709d5e5e97147dbb30e182d8613f94e
alevani <vanini.alexandre@gmail.com>
2023-12-04 09:39:58 +0100

    Day04 - part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -7,8 +7,8 @@ fn main() {
     // File hosts.txt must exist in the current path
     if let Ok(lines) = read_lines("./input.txt") {
         let input = lines.flatten().collect();
-        part_one(input);
-        // part_two(input);
+        // part_one(input);
+        part_two(input);
     }
 }
 
@@ -47,7 +47,54 @@ fn part_one(input: Vec<String>) {
     println!("result: {result}");
 }
 
-fn part_two(input: Vec<String>) {}
+fn part_two(input: Vec<String>) {
+    let mut number_of_scratch_cards = 0;
+
+    let mut numbers_of_matches_per_scratch_cards: Vec<i32> = Vec::new();
+    let mut numbers_of_copies_per_scratch_cards: HashMap<usize, i32> = HashMap::new();
+    for (index, line) in input.iter().enumerate() {
+        let mut newline = line.split(": ");
+        let _ = newline.next(); // Skips "Card   1: "
+
+        let mut card = newline.next().unwrap().split(" | ");
+
+        let winning_numbers: HashMap<i32, i32> = card
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .map(|f| (f.parse::<i32>().unwrap(), 0))
+            .collect();
+        let my_numbers = card
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .map(|f| f.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        let points = my_numbers
+            .iter()
+            .filter_map(|n| winning_numbers.get(n))
+            .count();
+
+        numbers_of_matches_per_scratch_cards.push(points as i32);
+        numbers_of_copies_per_scratch_cards.insert(index, 1);
+    }
+
+    for (index, n_of_m) in numbers_of_matches_per_scratch_cards.into_iter().enumerate() {
+        let numbers_of_copy = numbers_of_copies_per_scratch_cards.get(&index).unwrap();
+        for _ in 0..*numbers_of_copy {
+            for i in index + 1..index + 1 + n_of_m as usize {
+                numbers_of_copies_per_scratch_cards
+                    .entry(i)
+                    .and_modify(|nc| *nc += 1);
+            }
+        }
+    }
+
+    let result: i32 = numbers_of_copies_per_scratch_cards.values().sum();
+
+    println!("result: {result:?}");
+}
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
