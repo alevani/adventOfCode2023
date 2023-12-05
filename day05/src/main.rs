54faad3cda6922709b0488d1805944965c84f784
alevani <vanini.alexandre@gmail.com>
2023-12-05 12:10:32 +0100

    Empty day05

---
+++ day05/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    // File hosts.txt must exist in the current path
+    if let Ok(lines) = read_lines("./input.txt") {
+        let input = lines.flatten().collect();
+        part_one(input);
+        // part_two(input);
+    }
+}
+
+fn part_one(input: Vec<String>) {
+
+}
+
+fn part_two(input: Vec<String>) {
+
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
